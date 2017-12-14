b04de7893eca34046aee045a6d756819ba8e951d
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2017-12-14 13:10:12 +0000

    Setup GraphQL p[lugin for vscode, requires Watchman installed, changed file extensions to qgl for syntax highlighting and autocompletion of types

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",    
+    "extends": "airbnb-base",
     "plugins": [
         "import"
     ],
     "rules": {
         "semi": [2, "never"],
         "linebreak-style": ["error", "windows"],
-        "indent": [ 2, 4 ],
-        "eol-last": [ "error", "never"],
+        "indent": [2, 4],
+        "eol-last": ["error", "never"],
         "function-paren-newline": ["error", "never"],
         "no-use-before-define": ["error", {
             "functions": false,
             "classes": true
         }]
-    }
+    },
 }
